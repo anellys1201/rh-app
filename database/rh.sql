7e0a39b1a622094f8bb714f4ce293bf9625ebf07
Anelly Hernandez <anellys0303@gmail.com>
2026-03-09 14:20:24 -0600

    dump de la base de datos en mysql

--- database/rh.sql
+++ database/rh.sql
@@ -16,6 +16,61 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `bien`
+--
+
+DROP TABLE IF EXISTS `bien`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bien` (
+  `id_bien` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_bien`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bien`
+--
+
+LOCK TABLES `bien` WRITE;
+/*!40000 ALTER TABLE `bien` DISABLE KEYS */;
+INSERT INTO `bien` VALUES (1,'Laptop negra DELL ','Laptop Dell'),(2,'Teclado mecanico DELL','Teclado Dell'),(3,'Mouse negro Lenovo','Mouse Lenovo'),(4,'Monitor negro Dell','Monitor DELL'),(5,'NoBreak negro de 5 cargas','NoBreak');
+/*!40000 ALTER TABLE `bien` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_bien`
+--
+
+DROP TABLE IF EXISTS `detalle_bien`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_bien` (
+  `consec` int NOT NULL AUTO_INCREMENT,
+  `comentario` varchar(255) DEFAULT NULL,
+  `id_bien` int DEFAULT NULL,
+  `id_empleado` int DEFAULT NULL,
+  PRIMARY KEY (`consec`),
+  KEY `FKtei20cnfc2cn7n6c6qjrua2j6` (`id_bien`),
+  KEY `FKbk3vn7scnkkvua2wudmw31s3y` (`id_empleado`),
+  CONSTRAINT `FKbk3vn7scnkkvua2wudmw31s3y` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`),
+  CONSTRAINT `FKtei20cnfc2cn7n6c6qjrua2j6` FOREIGN KEY (`id_bien`) REFERENCES `bien` (`id_bien`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_bien`
+--
+
+LOCK TABLES `detalle_bien` WRITE;
+/*!40000 ALTER TABLE `detalle_bien` DISABLE KEYS */;
+INSERT INTO `detalle_bien` VALUES (7,'Laptop negra para trabajar en casa',1,7),(8,'Teclaro mecanico para la computadora de escritorio de la oficina',2,8),(9,'Otro monitor para trabajar comodo en la oficina',4,8),(10,'Para que no se apaguen las compus',5,8);
+/*!40000 ALTER TABLE `detalle_bien` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `empleado`
 --
 
@@ -24,11 +79,12 @@ DROP TABLE IF EXISTS `empleado`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `empleado` (
   `id_empleado` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) DEFAULT NULL,
   `departamento` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
   `sueldo` double DEFAULT NULL,
+  `documento` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_empleado`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +93,7 @@ CREATE TABLE `empleado` (
 
 LOCK TABLES `empleado` WRITE;
 /*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
-INSERT INTO `empleado` VALUES (1,'Anelly Hernandez','Banco de datos',28000),(2,'Juan Perez','Finanzas',35000),(5,'Pedro Fernandez','Recursos Minerales',59000);
+INSERT INTO `empleado` VALUES (7,'Banco de Datos','Anelly Schajris',1200,'e5dabb37-b8f0-41ac-998c-63e5084acdaf_05-01-00-Operadores-UJ.pdf'),(8,'Contabilidad','Juan Perez',2010,'1b2c727f-0f92-41a0-b642-ea7196ef2b76_06-12-00-OperadorTernario-UJ.pdf');
 /*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,8 +106,8 @@ DROP TABLE IF EXISTS `usuarios`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -62,7 +118,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'admin','admin1');
+INSERT INTO `usuarios` VALUES (1,'admin','admin');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -75,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-26 10:57:29
+-- Dump completed on 2026-03-09 14:19:15
